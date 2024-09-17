38799e452d4a95be026c2353dc836d77b91a8438
Heberth López <heberth.lopez.19@gmail.com>
2024-09-17 17:39:08 -0500

    chore: Added some comments

--- main.js
+++ main.js
@@ -1,28 +1,41 @@
 const contacts = [];
 const addButton = document.getElementById('addContact');
+const inputNewContact = document.getElementById('contactName');
 
-function addContact(contactName) {
+const contactCounter = document.createElement('div');
+contactCounter.textContent = `Total de contactos: ${contacts.length}`;
+contactCounter.style.marginBlock = '1rem';
+inputNewContact.after(contactCounter);
+
+function renderContact(contactName) {
   const contactList = document.getElementById('contactList');
-  const contactDiv = document.createElement('li');
-  contactDiv.style.display = 'flex';
-  contactDiv.style.gap = '1rem';
-  contactDiv.style.alignItems = 'center';
 
+  // Creando el elemento li para el contacto:
+  const contactLi = document.createElement('li');
+  contactLi.style.display = 'flex';
+  contactLi.style.gap = '1rem';
+  contactLi.style.alignItems = 'center';
+
+  // Creando el elemento span con el nombre del contacto:
   const contactSpan = document.createElement("span");
   contactSpan.textContent = `🎱 ${contactName}`;
-  contactDiv.appendChild(contactSpan);
-  contactList.appendChild(contactDiv);
+  contactLi.appendChild(contactSpan);
 
+  // Creando el botón para editar el contacto:
   const editButton = document.createElement('button');
-  editButton.className = 'editButton';
-
-  const deleteButton = document.createElement('button');
   editButton.textContent = 'Editar';
+  contactLi.appendChild(editButton);
+
+  // Creando el botón para borrar el contacto:
+  const deleteButton = document.createElement('button');
   deleteButton.textContent = 'Eliminar';
-  contactDiv.appendChild(editButton);
-  contactDiv.appendChild(deleteButton);
+  contactLi.appendChild(deleteButton);
+
+  // Agregando los eventos al elemento li:
   deleteButton.addEventListener('click', deleteContact);
   editButton.addEventListener('click', editContact);
+
+  contactList.appendChild(contactLi);
 }
 
 addButton.addEventListener('click', () => {
@@ -41,9 +54,13 @@ addButton.addEventListener('click', () => {
   }
 
   contacts.push(contactName);
-  addContact(contactName);
+  renderContact(contactName);
+
+  // Limpia el campo de texto
   const inputAddContact = document.getElementById('contactName');
   inputAddContact.value = '';
+
+  contactCounter.textContent = `Total de contactos: ${contacts.length}`;
 })
 
 function deleteContact(ev) {
@@ -72,8 +89,12 @@ function updateContact(ev) {
   const editButton = contactToEdit.children[1];
 
   const contactName = contactToEdit.children[0].value;
+
+  // Crea un span con el nuevo nombre de contacto
   const contactUpdated = document.createElement('span');
   contactUpdated.textContent = contactName;
+
+  // Reemplaza el input por el span con el nuevo contacto
   contactToEdit.replaceChild(contactUpdated, contactToEdit.firstChild);
   contactToEdit.children[1].textContent = 'Editar';
 
